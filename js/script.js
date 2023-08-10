f0ab93cb8698b12ac88c00144648733c9716e275
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2023-08-10 05:59:53 +0100

    Footer Section Design and Body Layout Tweak - #0005

--- js/script.js
+++ js/script.js
@@ -34,3 +34,52 @@ const presalePriceLabel = document.querySelector("#presale-price");
 const priceArr = [0.6, 0.25];
 icoPriceLabel.textContent = `$${priceArr[0]}`;
 presalePriceLabel.textContent = `$${priceArr[1]}`;
+
+
+/*  Token Allocation  */
+const allocation = [
+  {
+    title: "Team",
+    colour: "#0023ff",
+    percentage: "5%",
+  },
+  {
+    title: "Marketing",
+    colour: "#5af30f",
+    percentage: "15%",
+  },
+  {
+    title: "Liquidity",
+    colour: "#00fdff",
+    percentage: "10%",
+  },
+  {
+    title: "Private",
+    colour: "#afad4f",
+    percentage: "3%",
+  },
+  {
+    title: "Pre-Sale",
+    colour: "#7088ff",
+    percentage: "7%",
+  },
+  {
+    title: "Public",
+    colour: "#aa08ff",
+    percentage: "60%",
+  },
+];
+
+const allocationBar = document.querySelector(".allocation-bar");
+const allocationList = document.querySelector(".allocation-list");
+
+const tokenAllocation = function (data) {
+  data.forEach(el => {
+    const bar = `<div class="bar" style="background-color: ${el.colour}; flex: ${el.percentage};"></div>`;
+    const item = `<div class="value"><i style="background-color: ${el.colour};"></i>${el.title} (${el.percentage})</div>`;
+    allocationBar.insertAdjacentHTML("beforeend", bar);
+    allocationList.insertAdjacentHTML("beforeend", item)
+  });
+};
+
+tokenAllocation(allocation);
